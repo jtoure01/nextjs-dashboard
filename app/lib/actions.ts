78e65ec33121c0ca8ae5213d6648bed336b48d22
jtoure01 <jtoure001@gmail.com>
2024-02-07 07:23:22 +0000

    nextjs

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -3,6 +3,8 @@ import { z } from 'zod';
 import { sql } from '@vercel/postgres';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
+import { signIn } from '@/auth';
+import { AuthError } from 'next-auth';
 const FormSchema = z.object({
   id: z.string(),
   customerId: z.string(),
@@ -20,10 +22,16 @@ export async function createInvoice(formData: FormData) {
       });
       const amountInCents = amount * 100;
       const date = new Date().toISOString().split('T')[0];
-      await sql`
-      INSERT INTO invoices (customer_id, amount, status, date)
-      VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
-    `;
+      try {
+        await sql`
+          INSERT INTO invoices (customer_id, amount, status, date)
+          VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+        `;
+      } catch (error) {
+        return {
+          message: 'Database Error: Failed to Create Invoice.',
+        };
+    }
     revalidatePath('/dashboard/invoices');    
     redirect('/dashboard/invoices');
 }
@@ -35,17 +43,42 @@ export async function updateInvoice(id: string, formData: FormData) {
     });
    
     const amountInCents = amount * 100;
-   
+   try{
     await sql`
       UPDATE invoices
       SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
       WHERE id = ${id}
-    `;
+    `;}catch (error) {
+        return { message: 'Database Error: Failed to Update Invoice.' };
+      }
    
     revalidatePath('/dashboard/invoices');
     redirect('/dashboard/invoices');
   }
   export async function deleteInvoice(id: string) {
+    try{
     await sql`DELETE FROM invoices WHERE id = ${id}`;
+    }catch (error) {
+        return { message: 'Database Error: Failed to Delete Invoice.' };
+      }
     revalidatePath('/dashboard/invoices');
+  }
+
+  export async function authenticate(
+    prevState: string | undefined,
+    formData: FormData,
+  ) {
+    try {
+      await signIn('credentials', formData);
+    } catch (error) {
+      if (error instanceof AuthError) {
+        switch (error.type) {
+          case 'CredentialsSignin':
+            return 'Invalid credentials.';
+          default:
+            return 'Something went wrong.';
+        }
+      }
+      throw error;
+    }
   }
